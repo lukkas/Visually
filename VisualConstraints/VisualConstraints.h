da98dc3502939f501e321763cd357b544540562b
Lukasz Kasperek <luk.kasperek@gmail.com>
2018-03-17 15:08:40 +0100

    Initial Commit

---
+++ VisualConstraints/VisualConstraints.h
@@ -0,0 +1,19 @@
+//
+//  VisualConstraints.h
+//  VisualConstraints
+//
+//  Created by Łukasz Kasperek on 17.03.2018.
+//  Copyright © 2018 AppUnite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VisualConstraints.
+FOUNDATION_EXPORT double VisualConstraintsVersionNumber;
+
+//! Project version string for VisualConstraints.
+FOUNDATION_EXPORT const unsigned char VisualConstraintsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VisualConstraints/PublicHeader.h>
+
+
